03fcdd8780fe3bc433031cce2af506d65324b621
ThatAnnoyingKid <57725498+ThatAnnoyingKid@users.noreply.github.com>
2026-06-09 18:01:45 +1000

    Added bad words filtering
    There will be no swearing in childerns' Discord servers.

---
+++ christan.py
@@ -0,0 +1,10 @@
+import discord
+
+# There will be no bad words in my Christan Discord server
+with open('bad_words.txt', 'r') as file:
+    words_list = file.read().split()
+
+def no_bad_words(message):
+    for word in words_list:
+         if word in message.content:
+              return True
